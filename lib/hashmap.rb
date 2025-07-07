e7cfa965fa2ad9e61e33e46be65815b516cc401c
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-07 16:30:49 -0400

    feat: expand hashmap constructor

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,12 +1,15 @@
+require_relative "linked_list"
+
 # A blueprint for instantiating a new hashmap. When a key hashes to a certain
 # bucket index, it and its value will be prepended to the linked list contained
 # there in the form of a node containing a two-index array.
 class HashMap
-  attr_accessor :capacity
+  attr_accessor :capacity, :buckets
   attr_reader :load_factor
 
   def initialize
     @capacity = 16
     @load_factor = 0.75
+    @buckets = Array.new(capacity, LinkedList.new)
   end
 end
