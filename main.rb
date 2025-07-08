ce943e9c081526550ae2bcd69d4ffb182ad40293
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-08 00:24:41 -0400

    feat: add driver code in main file

---
+++ main.rb
@@ -0,0 +1,11 @@
+require_relative "lib/hashmap"
+
+h = HashMap.new
+h.set("Carlos", "Jacobs")
+h.set("Amanda", "Winthrop")
+h.set("Carlos", "Adams")
+h.set("Amanda", "Rose")
+p h.get("Carlos")
+p h.get("Amanda")
+p h.get("Josh")
+puts h
