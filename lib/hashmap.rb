85f02af127ce846ac36fcb8526b72ad1f1740d7b
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-08 19:08:12 -0400

    fix: hashmap require statement

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,4 +1,4 @@
-require_relative "linked_list"
+require_relative "bucket"
 
 # A blueprint for instantiating a new hashmap. When a key hashes to a certain
 # bucket index, it and its value will be prepended to the linked list contained
